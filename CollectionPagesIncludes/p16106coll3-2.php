1e7b1f904189d17b4027dc36c5451b96f91186a0
M Ryan Hess <pantosj@gmail.com>
2013-04-17 17:12:02 -0500

    New collection and includes
    Added Children's Magazine custom page and PHP includes files used for
    custom page references in CDM.

---
+++ CollectionPagesIncludes/p16106coll3-2.php
@@ -0,0 +1,12 @@
+
+<?php echo "<style type="text/css">";
+
+include 'http://libguides.depaul.edu/loader.php?type=d&id=731553'; 
+
+echo "</style>";
+
+echo "<script type="text/javascript">"
+
+include ''; ?></script>
+<?php include 'http://libguides.depaul.edu/loader.php?type=d&id=731283'; ?>
+?>
---
+++ CollectionPagesIncludes/p16106coll3.php
@@ -0,0 +1,2 @@
+<style type="text/css"><?php include 'http://libguides.depaul.edu/loader.php?type=d&id=731553'; ?></style>
+<?php include 'http://libguides.depaul.edu/loader.php?type=d&id=731283'; ?>
