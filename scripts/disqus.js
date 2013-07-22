2979ddf2418918f44d778a9324344b11a8456372
Jim LeFager <jameslefager@gmail.com>
2013-07-22 08:23:18 -0500

    Addition of Disqus plugin script
    Addition of disqus.js, which allows for including commenting
    functionality with ContentDM managed through Disqus

---
+++ scripts/disqus.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+//Adding DisQus Plugin to CONTENTdm
+//Styling of the plugin is managed by the disqus.css 
+//CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE
+//Include in the 
+var disqus_shortname = 'dpudigicoll'; // required: replace example with your forum shortname
+// Insert Disqus thread after details area in CDM
+$('#details').after('<div id="disqus_thread"></div>'); 
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+      
+       
